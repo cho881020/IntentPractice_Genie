5d8d0fb7bf52a676f8d7dbd7a8e3ab2507219a31
Geine <cho881020@gmail.com>
2021-04-07 12:43:22 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult) 활용

--- app/src/main/java/com/neppplus/intentpractice_genie/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice_genie/EditNicknameActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.intentpractice_genie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

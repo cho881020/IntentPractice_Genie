ec9e3c8c2e83fb7078318b7c637bc8f79f7be9c2
Geine <cho881020@gmail.com>
2021-04-08 10:19:59 +0900

    CALL 액션 예제 구현  - 권한을 얻지 못해서 에러 발생. (앱이 죽음)

--- app/src/main/java/com/neppplus/intentpractice_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice_genie/MainActivity.kt
@@ -15,6 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        callBtn.setOnClickListener {
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+
+            startActivity(myIntent)
+        }
+
 //        DIAL 액션 예제
 
         dialBtn.setOnClickListener {

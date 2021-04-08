959297aa2fccf9644f01249225f3f438463a6698
Geine <cho881020@gmail.com>
2021-04-08 10:33:00 +0900

    VIEW 액션 예제  - 인터넷 링크  - 플레이스토어 상세화면 링크

--- app/src/main/java/com/neppplus/intentpractice_genie/MainActivity.kt
+++ app/src/main/java/com/neppplus/intentpractice_genie/MainActivity.kt
@@ -15,6 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        kakaoStoreBtn.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+
+        }
+
+        naverWebBtn.setOnClickListener {
+            val myUri = Uri.parse("https://naver.com")
+
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+
         smsBtn.setOnClickListener {
 
             val inputPhoneNum = phoneNumEdt.text.toString()
